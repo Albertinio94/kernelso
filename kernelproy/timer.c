fa33ce4af102018641166f9297c5226347e6067c
Albertinio94 <albertorugbier@hotmail.es>
2022-11-03 16:57:36 +0100

    implementado timer del pgb, arreglados algunos bugs

--- kernelproy/timer.c
+++ kernelproy/timer.c
@@ -8,9 +8,24 @@ void timer_scheduler_routine(pthread_mutex_t *mutex, pthread_cond_t *condicion1,
         cuentapulsos++;
         if(cuentapulsos=10){
             printf("Soy el scheduler y he contado 10 pulsos\n");
+            cuentapulsos=0;
         }
         pthread_cond_signal(condicion1);
         pthread_cond_wait(condicion2, mutex);
-
+    }
+}
+void timer_process_generator_routine(pthread_mutex_t *mutex, pthread_cond_t *condicion1, pthread_cond_t *condicion2, int *done){
+    pthread_mutex_lock(mutex);
+    int cuentapulsos=0;
+    while(1){
+        *done++;
+        cuentapulsos++;
+        if(cuentapulsos=50){
+            printf("SOY EL PROCESS GENERATOR Y HE CONTADO 50 PULSOS\n");
+            cuentapulsos=0;
+            
+        }
+        pthread_cond_signal(condicion1);
+        pthread_cond_wait(condicion2, mutex);
     }
 }
