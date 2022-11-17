d98c9101e7b2cee4027d804b24185239a3e7f535
Albertinio94 <albertorugbier@hotmail.es>
2022-11-17 16:55:33 +0100

    Creado nodo y pcb

--- kernelproy/structures.h
+++ kernelproy/structures.h
@@ -1,6 +1,7 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 #include <pthread.h>
+
 typedef struct{
     int id;
     int tiempo_de_vida;
@@ -13,12 +14,12 @@ typedef struct {
 }
 nodo;
 
-struct common_args{
+struct argumentosparaclockytimer{
     pthread_mutex_t mutex;
-    pthread_cond_t condition1;
-    pthread_cond_t condition2;
+    pthread_cond_t condicion1;
+    pthread_cond_t condicion2;
     int done;
-    int n_timers;
+    int cantidad_temporizadores;
 };
-typedef struct common_args common_args;
+typedef struct argumentosparaclockytimer argumentosparaclockytimer;
 #endif
